22977d949139005460e1d4f09101b95048cf0a45
Elaine Ho <elaine48e@gmail.com>
2019-05-29 19:00:30 -0700

    gui fixes

--- Milestone 2/main.py
+++ Milestone 2/main.py
@@ -9,34 +9,31 @@ def search_and_show():
     
     ranked_results = searcher.find(query)
     
-    count = 0
     output = []
     for result in ranked_results:
-        if count>20:
-            break
-        output.append(result)
-        count+=1
+        output.append(result[0])
     scrollbar = Scrollbar(m)
     scrollbar.pack(side = RIGHT, fill = Y)
     mylist = Listbox(m, yscrollcommand = scrollbar.set)
     for line in output:
         mylist.insert(END, str(line))
-    mylist.pack( side = LEFT, fill = BOTH )
+    mylist.pack( side = TOP, fill = BOTH )
     scrollbar.config(command = mylist.yview)
     mainloop()
     
+
 if __name__ == "__main__":
     client = MongoClient(port=27017)
     if not 'CS121Project3' in client.list_database_names():
         Index().loop_urls(client)
 
-    database = client["CS121Project3"]["ICSindex"]
+    database = client["CS121Project3"]
     searcher = Searcher(database)
     
     m = tkinter.Tk()
-    Label(m, text='Enter a query:').pack(side=LEFT, fill=BOTH)
+    Label(m, text='Enter a query:').pack(side=TOP, fill=BOTH)
     query_box = Entry(m)
     submit_button = Button(m, text="Search", command=search_and_show)
-    query_box.pack(side = LEFT, fill = BOTH)
-    submit_button.pack(side = LEFT, fill = BOTH)
+    query_box.pack(side = TOP, fill = BOTH)
+    submit_button.pack(side = TOP, fill = BOTH)
     m.mainloop()
